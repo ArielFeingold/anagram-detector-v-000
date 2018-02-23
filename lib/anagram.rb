55def49b7f2a80a393835625549bb7c341bcdb96
Ariel Feingold <feingold.ariel@gmail.com>
2018-02-23 02:48:21 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
 # Your code goes here!
+# require 'pry'
+class Anagram
+  attr_accessor :word, :word
+  @@anagram_array = []
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(compare_array)
+    compare_array.each do |w|
+      if  w.split(//).sort == @word.split(//).sort
+        @@anagram_array << w
+      end
+    end
+    @@anagram_array
+  end
+
+end
